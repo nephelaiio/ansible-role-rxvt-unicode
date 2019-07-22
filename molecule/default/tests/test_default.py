4a68fc062a0fbae633f206880425bf9aac0aacb5
Ted Cook <teodoro.cook@gmail.com>
2019-07-21 23:13:22 -0600

    Fix test syntax error

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_command(host.command):
+def test_command(host):
     assert host.command('urxvt -h').rc == 1
     assert host.command('xrdb -help').rc == 1
